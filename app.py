a353a756ceae42be296cc469675fae34c9e171bd
HARSHA VARDHAN REDDY <61946603+algonoxharshareddy@users.noreply.github.com>
2022-03-31 13:12:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
-
-@app.route('/hello')
-def hello_flask():
-    return 'Hello from Docker Flask Application'
+    return 'Hello World, from Docker'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
